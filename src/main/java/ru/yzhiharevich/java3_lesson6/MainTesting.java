be91482d47f7d035cc3a1a93d15be1e9d66127b8
yury <zhurikk@ya.ru>
2018-12-09 17:13:32 +0300

    Lesson3

--- src/main/java/ru/yzhiharevich/java3_lesson6/MainTesting.java
+++ src/main/java/ru/yzhiharevich/java3_lesson6/MainTesting.java
@@ -1,4 +1,47 @@
 package ru.yzhiharevich.java3_lesson6;
 
 public class MainTesting {
+    public static int NUMBER = 4;
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{4, 3, 3, 3, 5, 6, 9};
+        int[] arr1 = new int[]{4, 1, 4, 1, 1, 1, 4};
+        getSymbolsAfterFour(arr);
+        checkArrayWithOneAndFour(arr1);
+    }
+
+    public static int[] getSymbolsAfterFour(int[] fullArray) {
+        int index = 0;
+        for (int i = 0; i < fullArray.length; i++) {
+            if (fullArray[i] == NUMBER) {
+                index = i + 1;
+            }
+        }
+        if (index == 0) {
+            throw new RuntimeException("Числа  " + NUMBER + " нет в массиве");
+        }
+
+        int[] newArray = new int[fullArray.length - index];
+        System.arraycopy(fullArray, index, newArray, 0, newArray.length);
+        return newArray;
+    }
+
+    public static boolean checkArrayWithOneAndFour(int[] arr) {
+        int fours = 0;
+        int ones = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 4) {
+                fours += 1;
+            } else if (arr[i] == 1) {
+                ones += 1;
+            } else if (arr[i] != 4 && arr[i] != 1) {
+                return false;
+            }
+        }
+        if (fours == ones) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/test/java/ru/yzhiharevich/marathon_lesson1/AppTest.java
+++ src/test/java/ru/yzhiharevich/marathon_lesson1/AppTest.java
@@ -1,20 +1,61 @@
 package ru.yzhiharevich.marathon_lesson1;
 
-import static org.junit.Assert.assertTrue;
+import static ru.yzhiharevich.java3_lesson6.MainTesting.getSymbolsAfterFour;
+import static ru.yzhiharevich.java3_lesson6.MainTesting.checkArrayWithOneAndFour;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+    private int[] arr = new int[]{5, 3, 5, 7, 8, 4, 2, 3};
+    private int[] arr1 = new int[]{5, 3, 5, 4, 8, 7, 2, 3};
+    private int[] arr2 = new int[]{4, 3, 4, 7, 8, 9, 2, 3};
+    private int[] arr3 = new int[]{5, 3, 5, 7, 8, 3, 2, 3};
+    private int[] arrFO = new int[]{1, 4, 4, 1, 1, 4, 4, 1};
+    private int[] arrFO1 = new int[]{1, 4, 4, 1, 1, 4, 4, 1};
+    private int[] arrFO2 = new int[]{1, 4, 4, 4, 4, 1, 1, 1, 1};
+    private int[] arrFO3 = new int[]{4, 4, 1, 1, 4, 4, 1};
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test1() {
+        int[] arrExcept1 = new int[]{2, 3};
+        Assert.assertArrayEquals(arrExcept1, getSymbolsAfterFour(arr));
+    }
+
+    @Test
+    public void test2() {
+        int[] arrExcept2 = new int[]{8, 7, 2, 3};
+        Assert.assertArrayEquals(arrExcept2, getSymbolsAfterFour(arr1));
+    }
+
+    @Test
+    public void test3() {
+        int[] arrExcept3 = new int[]{7, 8, 9, 2, 3};
+        Assert.assertArrayEquals(arrExcept3, getSymbolsAfterFour(arr2));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void test4() {
+        getSymbolsAfterFour(arr3);
+    }
+
+    @Test
+    public void testOneFour1() {
+        Assert.assertTrue(checkArrayWithOneAndFour(arrFO));
+    }
+
+    @Test
+    public void testOneFour2() {
+        Assert.assertTrue(checkArrayWithOneAndFour(arrFO1));
+    }
+
+    @Test
+    public void testOneFour3() {
+        Assert.assertTrue(checkArrayWithOneAndFour(arrFO2));
+    }
+
+    @Test
+    public void testOneFour4() {
+        Assert.assertTrue(checkArrayWithOneAndFour(arrFO3));
     }
 }
